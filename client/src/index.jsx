cdfb99bfa6e2a106d2e1d310676585f4f74bf92b
Jongerr <jontmichie@gmail.com>
2017-12-22 13:18:03 -0600

    Add form handling to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,10 +7,22 @@ class App extends React.Component {
     this.state = {name: 'Travis'};
   }
 
+  handleInputChange(e) {
+    this.setState({name: e.target.value});
+  }
+
+  handleSubmit(e) {
+    console.log('Hello,', this.state.name);
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div> 
-        <span>Hello: {this.state.name}</span>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input type="text" value={this.state.name} onChange={this.handleInputChange.bind(this)}/>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
