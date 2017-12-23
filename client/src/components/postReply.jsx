40a91f3ebcae86376a52e6b19160608853dc2aac
Jongerr <jontmichie@gmail.com>
2017-12-23 09:16:16 -0600

    Add elements and containers for the data in postReply react component.

--- client/src/components/postReply.jsx
+++ client/src/components/postReply.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const PostReply = (props) => (
-  <div>
-    {JSON.stringify(props.data)}
+  <div className="post-container">
+    <p className="post post-body">
+      {props.data.body}
+    </p>
+    <div className="post-legend">
+      <span className="post post-author">{props.data.author}</span>
+      <span className="post post-score">{props.data.score}</span>
+    </div>
   </div>
 );
   
